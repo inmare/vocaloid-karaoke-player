2a06c43225ebe7602f9db2a3c3b8ae107b92c02d
inmare <yatsumehole@naver.com>
2024-11-25 14:49:45 +0900

    get_song_data함수 추가

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 from tinydb import TinyDB, Query
 from pydantic import BaseModel, Field
+from word_compare import normalized_jamo_levenshtein
 
 db = TinyDB("db.json")
 
@@ -12,18 +13,24 @@ class SongData(BaseModel):
     mp3Link: str = Field(pattern=r"^https:\/\/.*$")
 
 
-# 더미 데이터
+def get_song_data(titleKR: str):
+    SongQuery = Query()
+    # 임의의 유사도 값으로 0.8을 설정함
+    compare = lambda t: normalized_jamo_levenshtein(titleKR, t) > 0.8
+    result = db.search(SongQuery.titleKR.test(compare))
+    return result
 
-# data = SongData(
-#     title="夜明けと蛍",
-#     titleKR="새벽과 반딧불",
-#     artist="n-buna",
-#     siteLink="https://piapro.jp/t/jEPl",
-#     mp3Link="https://cdn.piapro.jp/mp3_a/ko/ko4of47p1u5lalx4_20141111195715_audition.mp3",
-# )
 
-# db.insert(data.model_dump(mode="json"))
+if __name__ == "__main__":
+    pass
+    # 더미 데이터
 
-SongQuery = Query()
-result = db.search(SongQuery.titleKR == "새벽과 반딧불")
-print(result)
+    # data = SongData(
+    #     title="夜明けと蛍",
+    #     titleKR="새벽과 반딧불",
+    #     artist="n-buna",
+    #     siteLink="https://piapro.jp/t/jEPl",
+    #     mp3Link="https://cdn.piapro.jp/mp3_a/ko/ko4of47p1u5lalx4_20141111195715_audition.mp3",
+    # )
+
+    # db.insert(data.model_dump(mode="json"))
